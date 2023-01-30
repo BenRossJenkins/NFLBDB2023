f56a2636f377f1182b3b7d0d98a68f972d69be40
Ben Jenkins <48921076+BenRossJenkins@users.noreply.github.com>
2023-01-30 12:57:32 -0700

    Add files via upload

---
+++ CLIPPsack.R
@@ -0,0 +1,105 @@
+getwd()=='/Users/benjenkins'
+library(data.table)
+
+library(ggplot2)
+
+library(gt)
+library("gridExtra")   
+library(magick)
+library(data.table)
+library(caret)
+library(corrplot)
+library(yardstick) 
+library(tidyverse)
+library(gbm)
+library(visreg)
+library(MLeval)
+library(ROCR)
+library(iml)
+library(ggplot2)
+library(gganimate)
+theme_set(theme_bw())
+library(wesanderson)
+library(grid)
+library(ggvoronoi)
+
+
+
+
+df <- fread('Documents/nfl-big-data-bowl-2023/ackplay.csv')
+summary(df)
+
+#df <- df[df$gameId == 2021090900 & df$playId == 97,]
+
+#df <- df[df$pff_role == 'Pass Block']
+
+mids <- mean(df$CLIPPteam) 
+
+team <- ggplot(
+  df,
+  aes(frameId, CLIPPteam, group = frameId)) +
+  labs(x = "Play", y = "CLIPP")  +
+  theme(legend.position = "right") +
+  transition_reveal(frameId)  + 
+  theme(text = element_text(size = 20)) +
+  geom_point(aes(color = CLIPPteam), size=4) +
+  scale_color_gradient2(midpoint = mids, low = "red", mid = "yellow",
+                        high = "orange", space = "Lab" ) + 
+  geom_hline(yintercept = 0, color="black", size = 1) +
+  ggtitle('CLIPP of Entire Offensive Line', subtitle = 'Frame {frame} of {nframes}')
+
+team
+
+c_gif <- animate(team, width = 550, height = 550)
+
+
+
+mid <- mean(df$CLIPP) 
+
+
+sp2 <- ggplot(df, aes(y, x, group = displayName))+
+  geom_point(aes(color = CLIPP), size=4) +   
+  theme(text = element_text(size = 20)) +
+  #geom_hline(yintercept = 19, color="black")  +
+  scale_color_gradient2(midpoint = mid, low = "red", mid = "white",
+                        high = "green", space = "Lab" ) + 
+  transition_reveal(frameId) +
+  ggtitle('CLIPP of Offensive Line Movements', subtitle = 'Frame {frame} of {nframes}')  +
+  #view_follow(fixed_x = TRUE) + 
+  shadow_wake(wake_length = 1,
+              alpha = 1)+
+  labs(x = "Short Axis of Field (y) ", y = "Long Axis of Field (x)") 
+
+sp2
+
+
+a_gif <- animate(sp2, width = 550, height = 550)
+
+
+
+p <- ggplot(
+  df,
+  aes(frameId, CLIPP, group = displayName, color = displayName)) +
+  geom_line() +
+  scale_fill_manual(values = wes_palette("Rushmore1", n = 5)) +
+  labs(x = "Play", y = "CLIPP")  +
+  theme(text = element_text(size = 20)) +
+  theme(legend.position = "right") +
+  geom_point() + transition_reveal(frameId) + geom_hline(yintercept = 0, color="black", size = 1)  +
+  ggtitle('CLIPP of Each Offensive Linemen', subtitle = 'Frame {frame} of {nframes}')
+
+p
+
+b_gif <- animate(p, width = 550, height = 550)
+
+a_mgif <- image_read(a_gif)
+b_mgif <- image_read(b_gif)
+c_mgif <- image_read(c_gif)
+
+new_gif <- image_append(c(a_mgif[1], b_mgif[1], c_mgif[1]))
+for(i in 3:100){
+  combined <- image_append(c(a_mgif[i], b_mgif[i], c_mgif[i]))
+  new_gif <- c(new_gif, combined)
+}
+
+new_gif
